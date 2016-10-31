b81c92eb306791ade893f561dbf3b7994b0f2f53
Michelle Epstein <epstein.michelle@gmail.com>
2016-10-31 17:42:57 +0000

    added listing class

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,13 @@
-class Listing 
-end
+class Listing
+
+  attr_reader :spaces
+
+  def initialize
+    @spaces = []
+  end
+
+  def publish(space)
+    @spaces << space
+  end
+
+end 
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -1,3 +1,13 @@
 describe Listing do
 
+  let(:space) { double(:space) }
+  subject(:listing) { described_class.new}
+
+  context "publishing a space" do
+
+    it "can store a space" do
+      listing.publish(space)
+      expect(listing.spaces).to include space
+    end
+  end
 end
