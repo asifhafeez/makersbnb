301446c467d27951fb6e0b4c759b129673aaa9a6
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-11-02 10:24:48 +0000

    test

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -2,6 +2,6 @@ def add_space_form
   fill_in 'host',         with: 'Brian Chesky'
   fill_in 'email',        with: 'brianchesky@airbnb.com'
   fill_in 'name',        with: 'Watermarque'
-  fill_in 'price',        with: 10
+  fill_in 'price',        with: '10'
   fill_in 'description', with: 'Twin rooms'
 end
